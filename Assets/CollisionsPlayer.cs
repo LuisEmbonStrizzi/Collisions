7ec6c885c0716d90c47bcf892f2007df60aecb4a
LuisEmbonStrizzi <luisembonstrizzi@gmail.com>
2022-05-27 10:20:28 -0300

    Initial commit

---
+++ Assets/CollisionsPlayer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionsPlayer : MonoBehaviour
+{
+    Vector3 StartPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if(col.collider.name == "DeathWall")
+        {
+            transform.position = StartPos;
+        }
+        
+    }
+}
